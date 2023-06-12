97ce92e1d845754f3d028dc5183dfa961015f828
Javier Espitia <javi.espitiad@gmail.com>
2023-06-12 14:12:50 -0500

    implements validatios usign TDD

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,10 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+
+  has_many :posts
+
+  validates :email, presence: true
+  validates :auth_token, presence: true
+  validates :name, presence: true
 end
